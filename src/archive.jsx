acdfcbeec34e5fe649e1b18e45aa7131a600f4d4
Richard Saldivar <rsaldiva@gmail.com>
2018-04-12 17:08:14 +0000

    link to notFound component in archive.jsx

--- src/archive.jsx
+++ src/archive.jsx
@@ -53,17 +53,7 @@ class Archive extends React.Component {
     
     renderEmpty() {
         return (
-            <div id="content">
-        <div className="container post-entry">
-            <div className="card">
-                <div className="card-body">
-                    <h4 className="card-title">404 Page Not Found!</h4>
-                    <p className="card-text">The page you requested does not exist.</p>
-                    <p className="card-text"><Link to={CelestialSettings.path}>Return to homepage</Link></p>
-                </div>
-            </div>
-        </div>
-    </div>
+            <NotFound />
         )
     }
     
